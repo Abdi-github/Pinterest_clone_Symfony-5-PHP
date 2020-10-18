14436d1cf9b1a357b7228eb2ee146edbf431d1d6
abdigit <abdulkadirahmed1985@gmail.com>
2020-10-18 22:34:27 +0200

    PinsController cleaned up

--- src/Controller/PinsController.php
+++ src/Controller/PinsController.php
@@ -6,8 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\PinRepository;
 use App\Entity\Pin;
-//use Symfony\Component\Form\Extension\Core\Type\TextType;
-//use Symfony\Component\Form\Extension\C ore\Type\TextareaType;
+
 use Symfony\Component\HttpFoundation\Request;
 use Doctrine\ORM\EntityManagerInterface;
 use App\Form\PinType;
@@ -20,12 +19,7 @@ class PinsController extends AbstractController {
     public function index(PinRepository $pinRepo) {
 
 
-//       $pins = $pinRepo->findAll();
-        /*
-         * To display the pins by order of creation 
-         * (The Last entered the first displayed) we change
-         * the above findAll() method to findBy() method
-         */
+
 
         $pins = $pinRepo->findBy([], ['createdAt' => 'DESC']);
 
@@ -43,41 +37,7 @@ class PinsController extends AbstractController {
      * @Route("/pins/{id<[0-9]+>}/edit",name="app_edit", methods={"POST", "GET"})
      *
      */
-    /*
-      public function edit(Request $request, EntityManagerInterface $em, Pin $pin) {
-
-
-      $form = $this->createFormBuilder($pin)
-      ->add('title',TextType::class*)
-      ->add('description' ,TextareaType::class)
-      ->getForm()
-      ;
-
-      $form->handleRequest($request);
-
-
-      if ($form->isSubmitted() && $form->isValid()) {
-
-
-      $em->flush();
-
-      return $this->redirectToRoute('home');
-      //            return $this->redirectToRoute('pin.show', ['id' => $pin->getId()]);
-      }
-
-      return $this->render('pins/edit.html.twig', [
-      'pin' => $pin,
-      'editForm' => $form->createView()
-      ]);
-      }
-
-     */
-
-    /*
-     *  To simplify our code in edit function we can create form type on console
-     *  by make:form command and we create PinType.php on form directory and 
-     *  the edit function will be simplified as below
-     */
+    
 
     public function edit(Request $request, EntityManagerInterface $em, Pin $pin) {
 
@@ -99,7 +59,6 @@ class PinsController extends AbstractController {
             $this->addFlash('success', 'Pin updated successfully');
 
             return $this->redirectToRoute('app_home');
-//            return $this->redirectToRoute('pin.show', ['id' => $pin->getId()]);
         }
 
         return $this->render('pins/edit.html.twig', [
@@ -112,44 +71,7 @@ class PinsController extends AbstractController {
      * @Route("/pins/add",name="app_add", methods={"POST", "GET"})
      *
      */
-    /*
-      public function add(Request $request, EntityManagerInterface $em) {
-
-      $pin = new Pin;
-
-      $form = $this->createFormBuilder($pin)
-      ->add('title', TextType::class)
-      ->add('description', TextareaType::class)
-      ->getForm()
-      ;
-
-      $form->handleRequest($request);
-
-
-      if ($form->isSubmitted() && $form->isValid()) {
-
-
-      $em->persist($pin);
-      $em->flush();
-
-      return $this->redirectToRoute('home');
-      //            return $this->redirectToRoute('pin.show', ['id' => $pin->getId()]);
-      }
-
-
-
-      return $this->render('pins/add.html.twig', ['addForm' => $form->createView()]);
-      }
-
-     */
-
-
-
-    /*
-     *  To simplify our code in add function we can create form type on console
-     *  by make:form command and we create PinType.php on form directory and 
-     *  the add function will be simplified as below
-     */
+    
 
     public function add(Request $request, EntityManagerInterface $em) {
 
@@ -174,7 +96,6 @@ class PinsController extends AbstractController {
 
 
             return $this->redirectToRoute('app_home');
-//            return $this->redirectToRoute('pin.show', ['id' => $pin->getId()]);
         }
 
 
@@ -204,11 +125,7 @@ class PinsController extends AbstractController {
 
 
         return $this->redirectToRoute('app_home');
-//            return $this->redirectToRoute('pin.show', ['id' => $pin->getId()]);
     }
 
-    /*
-     * The above delete function works but we should use DELETE method instead of
-     * GET method so we should change the delete button in show.html.twig to form as shown
-     */
+    
 }
